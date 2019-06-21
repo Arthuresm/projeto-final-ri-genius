de0dd80a840c358709395412b1622a8063c6bfa6
bpoliana <bpolianajaber@gmail.com>
2019-06-20 21:35:51 -0300

    Adiciona script de coleta de musicas do Racionais MCs

---
+++ generate-corpus.py
@@ -0,0 +1,22 @@
+import lyricsgenius as genius
+api = genius.Genius('___your_genius_access_token_client_secret___')
+artist = api.search_artist('Racionais-mcs')
+lyrics = artist.save_lyrics()
+
+from glob import glob
+import json
+from pandas.io.json import json_normalize
+
+lyrics = []
+for f_name in glob('./*.json'):
+    config = json.loads(open(f_name).read())
+    lyrics.append(config)
+
+df = pd.DataFrame(lyrics)
+df.drop(['tracks','artist','audio_features' ], axis=1, inplace = True)
+df = df.dropna()
+df['title'] = df['songs'].apply(lambda x : x[0]['title'])
+df['album'] = df['songs'].apply(lambda x : x[0]['album'])
+df['lyrics'] = df['songs'].apply(lambda x : x[0]['lyrics'].lower())
+df.drop(['songs'], axis = 1, inplace = True)
+df.head()
